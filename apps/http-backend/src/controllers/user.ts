9df621ff4bf369cf21e876cc019f4d743bb94302
Taufiq <taufiq9.2004@gmail.com>
2025-04-24 20:51:13 +0530

    http signin/signup db

--- apps/http-backend/src/controllers/user.ts
+++ apps/http-backend/src/controllers/user.ts
@@ -2,24 +2,45 @@ import { Request, Response } from "express"
 import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/common-backend/config";
 import {CreateUserSchema, SigninSchema} from "@repo/common/valid"
+import { prisma } from "@repo/db/prima"
 
 
-export const signinHandler = (req:Request, res:Response) =>{
-    const data = SigninSchema.safeParse(req.body);
+export const signinHandler = async (req:Request, res:Response) =>{
+  const data = SigninSchema.safeParse(req.body);
    if(!data.success){
      res.json("input validation error")
    }
-const username = req.body.username
+const email = req.body.email
 const password = req.body.password
-//db check
-const userId= '';// return from db
+const user = await prisma.user.findUnique({
+  where: {email:email, password:password}
+})
+
+if(!user){
+  res.json("user not found")
+  return;
+}
+const userId = user.id;
+
 const token = jwt.sign(userId,JWT_SECRET)
 res.json(token);
 }
 
-export const signupHandler = (req:Request, res:Response) =>{
+export const signupHandler = async (req:Request, res:Response) =>{
    const data = CreateUserSchema.safeParse(req.body);
    if(!data.success){
      res.json("input validation error")
    }
+   const user = await prisma.user.create({
+    data: req.body
+   })
+   
+   if(!user){
+  res.json("user not found")
+  return;
+}
+const userId = user.id;
+
+const token = jwt.sign(userId,JWT_SECRET)
+res.json(token);
 }
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,7 +2,6 @@ import  express  from "express";
 import router from "./routes/base";
 import userRouter from "./routes/userRouter";
 import roomRouter from "./routes/roomRouter";
-import { prisma } from "@repo/db/prima"
 
 const app = express();
 app.use('/', router)
